dc1e02b526b905cc8b804d30e5848ce5f8c74659
aartuur <arturonoicencu@gmail.com>
2025-07-16 19:41:50 +0300

    ZAZAZA SAHUR

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import tweets
-from .routers import meme
+from routers import tweets
+from routers import meme
 
 app = FastAPI(title="Tweet Generator API")
 
