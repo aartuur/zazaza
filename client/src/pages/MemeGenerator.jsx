46eff8a1a5f454e1322b652237f20cdd418644f5
aartuur <arturonoicencu@gmail.com>
2025-07-16 19:47:13 +0300

    ZAZAZA SAHUR

--- client/src/pages/MemeGenerator.jsx
+++ client/src/pages/MemeGenerator.jsx
@@ -52,7 +52,7 @@ function MemeGenerator() {
         setPromptUsed('');
 
         try {
-            const response = await fetch(`https://sahurapi.onrender.com/meme-generator/`, {
+            const response = await fetch(`https://zazazaapi.onrender.com/meme-generator/`, {
                 method: 'POST',
                 credentials:"include",
                 headers: {
